bddbdb89481287869fd36c40bf373adb2448a81d
Antonio Saiful Islam <finallyantonio@gmail.com>
2023-03-28 21:47:46 +0700

    [add] register application.yml into config server

--- src/main/java/com/edustar/gateway/service/AuthService.java
+++ src/main/java/com/edustar/gateway/service/AuthService.java
@@ -1,11 +1,9 @@
 package com.edustar.gateway.service;
 
-import com.edustar.gateway.config.FeignConfig;
 import com.edustar.gateway.model.AuthTokenModel;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-
 
 
 @FeignClient(value = "${auth.serviceid}", url = "http://localhost:8888/")
